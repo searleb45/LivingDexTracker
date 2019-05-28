101482a948f6308a497a4f5c5a74fc82baec37ff
Bradley Searle <bradley.c.searle@accenture.com>
2019-05-28 15:59:43 -0500

    Convert site header to a component, add (very) basic CSS, start converting pokemon list to a more content-rich format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PkmnListComponent } from './pkmn-list/pkmn-list.component';
 import { PkmnDetailComponent } from './pkmn-detail/pkmn-detail.component';
 import { HomeComponent } from './home/home.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PkmnListComponent,
     PkmnDetailComponent,
-    HomeComponent
+    HomeComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule,
