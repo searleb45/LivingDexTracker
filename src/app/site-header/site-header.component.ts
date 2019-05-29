b18977f0c5dfcd3df649b18ba8532c2c4d8f9cb8
Bradley Searle <bradley.c.searle@accenture.com>
2019-05-29 15:17:49 -0500

    Sticky header, consolidate global styles to a single import file

--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -10,6 +10,8 @@ export class SiteHeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let headerSpacer: HTMLElement = document.querySelector('.header-spacer') as HTMLElement;
+    headerSpacer.style.height = document.querySelector('header').clientHeight + 'px';
   }
 
 }
